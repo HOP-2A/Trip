1a1258f0e5e33f2fd47622c40c7dd7fd2fddd3c7
tuuk <turuuzbi@gmail.com>
2025-11-25 14:07:30 +0800

    gg

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,14 +1,21 @@
 "use client";
 
-import { Input } from "@/components/ui/input";
-
 export const Header = () => {
   return (
-    <div className="flex justify-around fixed left-0 top-0 bg-white">
-      <div>Home</div>
-      <div>Trips</div>
-      <div>About us</div>
-      <Input placeholder="Search here..." />
+    <div className="fixed left-0 right-0 top-0 w-full z-1 ">
+      <div className="flex justify-around m-3">
+        <div className="flex justify-between w-70">
+          <div className="border  flex items-center font-semibold px-3  rounded-lg shadow-lg hover:scale-105 transition-transform text-white">
+            Home
+          </div>
+          <div className="border  flex items-center font-semibold px-3 rounded-lg shadow-lg hover:scale-105 transition-transform text-white">
+            Trips
+          </div>
+          <div className="border  flex items-center font-semibold px-3 rounded-lg shadow-lg hover:scale-105 transition-transform text-white">
+            About us
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,43 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Header } from "./_components/Header";
+
 export default function Home() {
   return (
-    <div>
-      <div>test</div>
+    <div className="min-h-screen bg-gray-100 relative">
+      <Header />
+      <div className="relative">
+        <img
+          src={"/nature.jpg"}
+          alt="Nature Photo"
+          className="w-full h-150 object-cover object-center"
+        />
+        <div className="flex flex-col gap-10 items-center justify-center absolute inset-0">
+          <h1 className="text-white text-5xl font-extrabold text-center px-4">
+            Plan Your Next Adventure
+          </h1>
+          <Button className="bg-gradient-to-r from-green-400 to-cyan-300 text-white font-semibold px-6 py-3 rounded-lg shadow-lg hover:scale-105 transition-transform">
+            Start Planning
+          </Button>
+        </div>
+      </div>
+
+      <div className="p-8 flex justify-center">
+        <Input
+          type="text"
+          placeholder="Search for a city or trip..."
+          className="w-2/3 sm:w-1/2 border border-gray-300 rounded-full px-6 py-3 shadow-md focus:outline-none focus:ring-2 focus:ring-indigo-400 transition"
+        />
+      </div>
+
+      <div className="p-8">
+        <h2 className="text-2xl font-bold mb-6 text-gray-700">
+          Featured Trips
+        </h2>
+      </div>
+      <div className="p-8">
+        <div className="text-2xl font-bold mb-6 text-gray-700">Your Trips</div>
+      </div>
     </div>
   );
 }
