9b7bc4bc74e9f3685b073899c5ece80f84232216
25HP1499@nest.edu.mn <25HP1499@pinecones-iMac.local>
2025-11-25 14:33:59 +0800

    very simple changes

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -4,14 +4,14 @@ export const Header = () => {
   return (
     <div className="fixed left-0 right-0 top-0 w-full z-1 ">
       <div className="flex justify-around m-3">
-        <div className="flex justify-between w-70">
-          <div className="border  flex items-center font-semibold px-3  rounded-lg shadow-lg hover:scale-105 transition-transform text-white">
+        <div className="flex justify-between w-70 font-semibold text-white">
+          <div className="border flex items-center px-3 rounded-lg shadow-lg hover:scale-125 transition-transform hover:cursor-pointer">
             Home
           </div>
-          <div className="border  flex items-center font-semibold px-3 rounded-lg shadow-lg hover:scale-105 transition-transform text-white">
+          <div className="border flex items-center px-3 rounded-lg shadow-lg hover:scale-125 transition-transform hover:cursor-pointer">
             Trips
           </div>
-          <div className="border  flex items-center font-semibold px-3 rounded-lg shadow-lg hover:scale-105 transition-transform text-white">
+          <div className="border flex items-center px-3 rounded-lg shadow-lg hover:scale-125 transition-transform  hover:cursor-pointer">
             About us
           </div>
         </div>
