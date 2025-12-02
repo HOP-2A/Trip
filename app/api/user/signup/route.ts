759cecd9e1703b00cbff8ab183796b86d60b85ff
turuuzbi <turuuzbi@gmail.com>
2025-12-02 17:38:59 +0800

    changed prisma, removed useless routes and pages

--- app/api/user/signup/route.ts
+++ app/api/user/signup/route.ts
@@ -1,14 +1,14 @@
 import { prisma } from "@/lib/db";
 import { NextResponse } from "next/server";
-import{ hash } from "bcrypt";
+import { hash } from "bcrypt";
 
 export const POST = async (req: Request) => {
   const body = await req.json();
   const { name, email, password } = body;
 
   const saltRounds = 10;
-  const hashedPassword = await hash(password, saltRounds)
-  
+  const hashedPassword = await hash(password, saltRounds);
+
   const createdUser = await prisma.user.create({
     data: {
       name,
@@ -16,7 +16,6 @@ export const POST = async (req: Request) => {
       password: hashedPassword,
     },
   });
+
   return NextResponse.json(createdUser);
 };
-
-
