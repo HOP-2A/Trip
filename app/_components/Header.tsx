06e68c8814578e880cb1ce9eddd71099f558d725
Bolorsaikhan <Boko@pinecones-iMac.local>
2025-12-02 18:31:14 +0800

    detailed main

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -10,21 +10,19 @@ import {
 
 export const Header = () => {
   return (
-    <div>
-      <div className="relative z-1">
-        <div className="absolute z-2">
-          <SignedOut>
-            <SignInButton />
-            <SignUpButton>
-              <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                Sign Up
-              </button>
-            </SignUpButton>
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-        </div>
+    <div className="relative z-1">
+      <div className="absolute z-2">
+        <SignedOut>
+          <SignInButton />
+          <SignUpButton>
+            <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+              Sign Up
+            </button>
+          </SignUpButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </div>
     </div>
   );
