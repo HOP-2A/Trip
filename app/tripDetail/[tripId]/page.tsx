37152baced468988e58b89abaff4eab22ea13ec3
25HP0700@nest.edu.mn <25hp0700nest.edu.mn@pinecones-iMac.local>
2025-12-13 11:19:26 +0800

    added a dynamic page for detailedTrip

--- app/tripDetail/[tripId]/page.tsx
+++ app/tripDetail/[tripId]/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Clock7, Clock8, MapPinned } from "lucide-react";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
@@ -20,14 +21,47 @@ const Page = () => {
     getTrips();
   }, [tripId]);
   console.log(trip);
-
   return (
     <div>
-      <div>{trip.title}</div>
-      <div>{trip.destination}</div>
-      <img src={trip.images[0]} />
-      <div>{trip.startDate}</div>
-      <div>{trip.endDate}</div>
+      <div className="w-full bg-[#f6f7f8] py-10">
+        <div className="w-full max-w-[1500px] mx-auto flex flex-col gap-6 px-8">
+          <div className="relative flex justify-center">
+            <div className="w-60 h-[500px] bg-gradient-to-tl from-[#07207a] to-transparent rounded-tl-2xl rounded-bl-2xl" />
+
+            <img
+              className="w-[820px] h-[500px] object-cover shadow-[0_8px_25px_rgba(0,0,0,0.25)]"
+              src={trip.images}
+              alt=""
+            />
+
+            <div className="w-60 h-[500px] bg-gradient-to-tr from-[#07207a] to-transparent rounded-tr-2xl rounded-br-2xl" />
+          </div>
+
+          <div className="bg-[#fbfbfb] py-4 px-4  rounded-xl shadow-sm w-[700] h-[200px]">
+            <div className="flex">
+              <div className="flex gap-4 items-center text-4xl font-extrabold tracking-tight text-[#203250]">
+                <MapPinned />
+                <span>{trip?.destination}</span>
+                <span>-</span>
+                <span>{trip?.title}</span>
+              </div>
+              <div>
+                <input type="text" />
+              </div>
+            </div>
+
+            <div className="flex gap-2">
+              <Clock7 className="h-5 w-5" />
+              <div className="text-gray-900">{trip?.startDate}</div>
+            </div>
+
+            <div className="flex gap-2">
+              <Clock8 className="h-5 w-5" />
+              <div className="text-gray-900">{trip?.endDate}</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
