a0157863cda6368b1804e247b943ed03be18d9ec
25HP0700@nest.edu.mn <25hp0700nest.edu.mn@pinecones-iMac.local>
2025-12-16 15:23:15 +0800

    travel uudig detail hiij haruljaga

--- app/tripDetail/[tripId]/page.tsx
+++ app/tripDetail/[tripId]/page.tsx
@@ -2,6 +2,7 @@
 import { Clock7, Clock8, MapPinned } from "lucide-react";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
+import Snowfall from "react-snowfall";
 
 const Page = () => {
   const [trip, setTrip] = useState({});
@@ -23,41 +24,52 @@ const Page = () => {
   console.log(trip);
   return (
     <div>
-      <div className="w-full bg-[#f6f7f8] py-10">
-        <div className="w-full max-w-[1500px] mx-auto flex flex-col gap-6 px-8">
+      <div className="w-full h-[100vh] relative w-full h-[100vh] bg-black py-10 overflow-hidden ">
+        <Snowfall
+          snowflakeCount={200}
+          color="white"
+          style={{
+            position: "absolute",
+            inset: 0,
+            zIndex: 0,
+            pointerEvents: "none",
+          }}
+        />
+         
+        <div className="relative z-10 w-full max-w-[1500px] mx-auto flex flex-col gap-6 px-8 ">
           <div className="relative flex justify-center">
-            <div className="w-60 h-[500px] bg-gradient-to-tl from-[#07207a] to-transparent rounded-tl-2xl rounded-bl-2xl" />
+            <div className="w-60 h-[500px] bg-[#c2e4f1] to-transparent rounded-tl-2xl rounded-bl-2xl" />
 
             <img
-              className="w-[820px] h-[500px] object-cover shadow-[0_8px_25px_rgba(0,0,0,0.25)]"
+              className="w-[1400px] h-[500px] object-cover shadow-[0_8px_25px_rgba(0,0,0,0.25)]"
               src={trip.images}
               alt=""
             />
+            
 
-            <div className="w-60 h-[500px] bg-gradient-to-tr from-[#07207a] to-transparent rounded-tr-2xl rounded-br-2xl" />
+            <div className="w-60 h-[500px] bg-[#c24c3a] to-transparent rounded-br-2xl rounded-tr-2xl" />
           </div>
 
-          <div className="bg-[#fbfbfb] py-4 px-4  rounded-xl shadow-sm w-[700] h-[200px]">
+          <div className="bg-[#fbfbfb] py-4 px-4  rounded-xl shadow-sm w-[700] h-[200px] space-y-2">
             <div className="flex">
-              <div className="flex gap-4 items-center text-4xl font-extrabold tracking-tight text-[#203250]">
+              <div className="flex gap-4 items-center text-4xl font-extrabold tracking-tight text-[#203250] ">
                 <MapPinned />
-                <span>{trip?.destination}</span>
-                <span>-</span>
-                <span>{trip?.title}</span>
-              </div>
-              <div>
-                <input type="text" />
+                <span className="leading-none">{trip?.destination}</span>
+                <div>-</div>
+                <div>{trip?.title}</div>
               </div>
             </div>
 
-            <div className="flex gap-2">
-              <Clock7 className="h-5 w-5" />
-              <div className="text-gray-900">{trip?.startDate}</div>
-            </div>
+            <div className="flex gap-3">
+              <div className="flex gap-2">
+                <Clock7 className="h-5 w-5" />
+                <div className="text-gray-900">{trip?.startDate}</div>
+              </div>
 
-            <div className="flex gap-2">
-              <Clock8 className="h-5 w-5" />
-              <div className="text-gray-900">{trip?.endDate}</div>
+              <div className="flex gap-2">
+                <Clock8 className="h-5 w-5" />
+                <div className="text-gray-900">{trip?.endDate}</div>
+              </div>
             </div>
           </div>
         </div>
